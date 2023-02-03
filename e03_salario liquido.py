cc4ab6cbaa21cf082aa92b303a85a8b16dd8bd12
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-03 19:24:29 -0300

    Criacao ex03

---
+++ e03_salario liquido.py
@@ -0,0 +1,27 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Faça um programa que pergunte quanto voce ganha por hora e o numero de horas trabaçhadas no mes.
+Calcule e mostre o total do seu salario no referido mes, sabendo-se que sao descontados 11% para 
+o Imposto de Renda, 8% para o INSS e 5% para o sindicato e faça conforme abaixo:
+a) salario bruto
+b) quanto pagou ao INSS
+c) quanto pagou ao sindicato
+d) o salario liquido
+e) calcule os descontos e o salario liquido
+'''
+qtyHoras = float(input('Quantidade de horas trabalhadas em 1 mes: '))
+valorHora = float(input('Valor por Hora trabalhada: '))
+sBruto = qtyHoras * valorHora
+ir = sBruto * 11/100
+inss = sBruto * 8 / 100
+sind = sBruto * 5 / 100
+descontos = ir + inss + sind
+print('='*45)
+print('DEMONSTRATIVO DE PAGAMENTO'.center(45))
+print('='*45)
+print(f'{"+ Salário Bruto":.<30}', f'R${sBruto:>12.2f}')
+print(f'{"- IR(11%)":.<30}', f'R${ir:>12.2f}')
+print(f'{"- INSS(8%)":.<30}', f'R${inss:>12.2f}')
+print(f'{"- Sindicato(5%)":.<30}', f'R${sind:>12.2f}')
+print()
+print(f'{"= Salario Liquido":.<30}', f'R${sBruto-descontos:>12.2f}')
