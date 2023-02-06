33dde0d9b6fceab6c16d23c901f358d68a8831e3
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-06 09:54:11 -0300

    Criacao ex19

---
+++ ex19_informacao de resultado.py
@@ -0,0 +1,40 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+ - par ou ímpar;
+ - positivo ou negativo;
+ - inteiro ou decimal.
+'''
+n1 = float(input('Digite o 1o. numero: '))
+n2 = float(input('Digite o 2o. numero: '))
+res = 0
+print("1 - Somar\n2 - Subtrair\n3 - Dividir\n4 - Multiplicar")
+while True:
+    op = str(input('Qual operação deseja realizar? ')).strip()[0]
+    if op in "1234":
+        break
+if op == "1":
+    res = n1 + n2
+    print(f'A soma entre {n1} e {n2} = {res}')
+elif op == "2":
+    res = n1 - n2
+    print(f'A subtração entre {n1} e {n2} = {res}')
+elif op == "3":
+    res = n1 / n2
+    print(f'A divisão entre {n1} e {n2} = {res}')
+elif op == "4":
+    res = n1 * n2
+    print(f'A multiplicação entre {n1} e {n2} = {res}')
+print(f'O resultado da operação indica que o numero é ', end="")
+if res % 2 == 0:
+    print('PAR, ', end="")
+else:
+    print('IMPAR, ', end="")
+if res > 0:
+    print('POSITIVO, ', end="")
+else:
+    print("NEGATIVO, ",end="")
+if res == round(res):
+    print('INTEIRO')
+else:
+    print('DECIMAL')
