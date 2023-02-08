e2a6f1c3ee0b220dbef6a278e54dc7089fa598fd
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-08 10:47:01 -0300

    Criacao ex27

---
+++ PARTE 2/ex27_aumento de populacao.py
@@ -0,0 +1,17 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Supondo que a população de um país A seja da ordem de 80.000 habitantes com uma taxa anual de crescimento de 3% 
+e que a população de B seja 200.000 habitantes com uma taxa de crescimento de 1.5%. 
+Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou 
+iguale a população do país B, mantidas as taxas de crescimento.
+'''
+a = 80000
+b = 200000
+anos = 0
+while True:
+    a += a * 3 / 100
+    b += b * 1.5 / 100
+    anos += 1
+    if a >= b:
+        break
+print(f'O país A levará {anos} anos para que se iguale ou supere a população do país B')
