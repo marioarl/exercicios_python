33ba4505312eec28ef74ca1f4712e9d179bfe352
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-08 22:04:25 -0300

    Criacao ex28

---
+++ PARTE 2/ex28_aumento de populacao V2.py
@@ -0,0 +1,61 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Faça um programa que o usuario digite o nome de 2 Paises (A e B), a população de dois paises e suas taxas de crescimento, 
+mostre o resultado de quantos anos o pais A se iguale ou ultrapasse o pais B na quantidade de população. E tambem que 
+o programa encerre quando o usuario informar NÃO em continuar.
+OBS: O programa deverá ser capaz de identificar se a populacao do pais A é maior que a do país B e 
+efetue os calculos corretos
+'''
+from os import system
+from time import sleep
+while True:
+    system('cls')
+    print('-='*40)
+    print('TAXA POPULACIONAL'.center(80))
+    print('-='*40)
+    while True:
+        paisA = str(input('Nome do país A: ')).strip()
+        if paisA.isalpha():
+            break
+        print('ERRO! Este campo deverá ter apenas LETRAS')      
+    popA = int(input(f'População do pais {paisA}: '))
+    taxaA = float(input(f'Taxa de crescimento do pais {paisA} (%): '))
+    print('.'*80)
+    
+    while True:
+        paisB = str(input('Nome do país B: ')).strip()
+        if paisB.isalpha():
+            break
+        print('ERRO! Este campo deverá ter apenas LETRAS')
+    popB = int(input(f'População do pais {paisB}: '))
+    taxaB = float(input(f'Taxa de crescimento do pais {paisB} (%): '))
+    anos = 0
+    while True:
+        if popA > popB:
+            popA += popA * taxaA / 100
+            popB += popB * taxaB / 100
+            anos += 1
+            if popB >= popA:
+                print('='*80)
+                print(f'{paisB} levará {anos} anos para que se iguale ou supere a população do país {paisA}')
+                print('='*80)
+                break
+        elif popB > popA:
+            popA += popA * taxaA / 100
+            popB += popB * taxaB / 100
+            anos += 1
+            if popA >= popB:
+                print('='*80)
+                print(f'{paisA} levará {anos} anos para que se iguale ou supere a população do país {paisB}')
+                print('='*80)
+                break
+    while True:
+        op = str(input('\nContinuar? [S/N]: ')).strip().upper()[0]
+        if op in "SN":
+            break
+        print('ERRO! Digite apenas S ou N')
+    if op in "N":
+        print('FINALIZANDO...')
+        sleep(1.5)
+        break
+print('OBRIGADO POR USAR O PROGRAMA! VOLTE SEMPRE!')
