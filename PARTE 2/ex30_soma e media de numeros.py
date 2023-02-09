3b6a2f464c567bdd4ac69b2b1cb2b6dc01222ff4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-09 07:49:12 -0300

    Criacao ex30

---
+++ PARTE 2/ex30_soma e media de numeros.py
@@ -0,0 +1,12 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Faça um programa que leia 5 números e informe a soma e a média dos números
+'''
+media = soma = 0
+
+for n in range(1,6):
+    num = int(input(f'Digite o {n}o. numero: '))
+    soma += num
+media = soma / 5
+print(f'A soma dos valores digitados é: {soma}')
+print(f'A média dos valores digitados é : {media:.2f}')
