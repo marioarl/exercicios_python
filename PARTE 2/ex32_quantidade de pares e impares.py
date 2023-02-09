baa620e5c2e59233041ac4605946e4d4f73becab
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-09 08:01:08 -0300

    Criacao ex32

---
+++ PARTE 2/ex32_quantidade de pares e impares.py
@@ -0,0 +1,13 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares e a quantidade de números impares.
+'''
+pares = impares = 0
+for n in range(1,11):
+    num = int(input(f'Digite o {n}o. numero: '))
+    if num % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+print(f'Quantidade de numeros PARES:   {pares}')
+print(f'Quantidade de numeros IMPARES: {impares}')
