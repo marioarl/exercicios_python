d015944e78919bae61f3bba4c24c1aaee4610b73
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-10 16:32:28 -0300

    Criacao ex38

---
+++ PARTE 2/ex38_media de temperaturas.py
@@ -0,0 +1,17 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia um conjunto indeterminado de temperaturas, e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
+'''
+temperaturas = []
+while True:
+    temperaturas.append(float(input('Digite a temperatura em graus Celcius: ')))
+    while True:
+        cont = str(input('Continuar [S/N] ')).strip().upper()[0]
+        if cont in "SN":
+            break
+        print('ERRO, Digite apenas S ou N')
+    if cont in "N":
+        break
+print(f'A MAIOR temperatura digitada foi {max(temperaturas)} graus')
+print(f'A MENOR temperatura digitada foi {min(temperaturas)} graus')
+print(f'A MÉDIA das temperaturas é {sum(temperaturas)/ len(temperaturas):.1f} graus')
