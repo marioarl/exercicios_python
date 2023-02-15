b447d3a469b4514394330141c29fdaee51ba36de
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-15 18:14:35 -0300

    Update

--- PARTE 2/ex34_apuracao de eleicao.py
+++ PARTE 2/ex34_apuracao de eleicao.py
@@ -3,7 +3,9 @@ Credito : https://wiki.python.org.br/EstruturaSequencial
 Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. 
 Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
 '''
+from colorama import init, Fore, Style
 from os import system
+init(autoreset=True)
 voto1 = voto2 = voto3 = 0
 candidato1 = "Toninho do Gas"
 candidato2 = "Denilson Chulé"
@@ -18,7 +20,7 @@ for e in range(1, numEleitores+ 1):
     print('='*50)
     print('SISTEMA ELEITORAL DA CIDADE DE RIO DOS CANTOS')
     print('='*50)
-    print(f'Escolha o candidato abaixo \033[31mELEITOR {e}\033[m:\n[ 1 ] - Toninho do Gas\n[ 2 ] - Denilson Chulé\n[ 3 ] - Anderson Sucatão')
+    print(f'Escolha o candidato abaixo {Fore.RED}ELEITOR {e}{Style.RESET_ALL}:\n[ 1 ] - Toninho do Gas\n[ 2 ] - Denilson Chulé\n[ 3 ] - Anderson Sucatão')
     voto = int(input('>>> '))
     if voto == 1:
         voto1 += 1
@@ -38,7 +40,7 @@ print(f'{candidato2:.<35}{" "}{voto2:<10}{(voto2 / numEleitores)*100:.1f}')
 print(f'{candidato3:.<35}{" "}{voto3:<10}{(voto3 / numEleitores)*100:.1f}')
 print()
 print('='*50)
-print(f'\033[31mFIM DA ELEIÇÃO\033[m'.center(58))
+print(f'{Fore.RED}FIM DA ELEIÇÃO'.center(58))
 print('='*50)
 
 
