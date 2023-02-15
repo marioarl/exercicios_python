42845020e296bd91665e8fc391511efe210b5f16
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-15 18:00:52 -0300

    Update

--- PARTE 2/ex26_cadastro com validacao.py
+++ PARTE 2/ex26_cadastro com validacao.py
@@ -7,6 +7,9 @@ Salário: maior que zero;
 Sexo: 'f' ou 'm';
 Estado Civil: 's', 'c', 'v', 'd';
 '''
+from colorama import Fore,init
+init(autoreset=True)
+
 print('='*30)
 print('SISTEMA DE CADASTRO'.center(30))
 print('='*30)
@@ -14,46 +17,46 @@ while True:
     nome = str(input('Nome: '))
     if len(nome) > 3:
         break
-    print('\033[31mERRO!! Nome deve ter mais de 3 caracteres\033[m')
+    print(Fore.RED + 'ERRO!! Nome deve ter mais de 3 caracteres')
 while True:
     try:
         idade = int(input('Idade:'))
     except ValueError:
-        print('\033[31mERRO!Campo deve ser preenchido\033[m')
+        print(Fore.RED + 'ERRO!Campo deve ser preenchido')
         continue
     if idade >=0 and idade <=150:
         break
-    print('\033[31mERRO! Idade de ser entre 0 e 150\033[m')
+    print(Fore.RED + 'ERRO! Idade de ser entre 0 e 150')
 
 while True:
     try:
         salario = float(input('Salário R$: '))     
     except ValueError:
-        print("\033[31mERRO! Campo salario deve ser preenchido\033[m")
+        print(Fore.RED + "ERRO! Campo salario deve ser preenchido")
         continue
     if salario > 0:
             break
-    print('\033[31mERRO! Salario deve ser maior que 0\033[m')
+    print(Fore.RED + 'ERRO! Salario deve ser maior que 0')
 
 while True:
     try:   
         sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
     except IndexError:
-        print("\033[31mERRO! Campo salario deve ser preenchido\033[m")
+        print(Fore.RED + "ERRO! Campo salario deve ser preenchido")
         continue
     if sexo in "MF":
         break
-    print('\033[31mERRO! Digite apenas M ou F\033[m')
+    print(Fore.RED + 'ERRO! Digite apenas M ou F')
 
 while True:
     try:
         estadoC = str(input('Estado Civil\n[ S ] - Solteiro(a) \n[ C ] - Casado(a)\n[ V ] - Viuvo(a)\n[ D ] - Divorciado(a)\n>>> ')).upper().strip()[0]
     except IndexError:
-        print("\033[31mERRO! Campo salario deve ser preenchido\033[m")
+        print(Fore.RED + "ERRO! Campo salario deve ser preenchido")
         continue
     if estadoC in 'SCVD':
         break
-    print('ERRO! Digite apenas "S", "C", "V", "D"')
+    print(Fore.RED + 'ERRO! Digite apenas "S", "C", "V", "D"')
 print(f'As informações digitadas foram:')
 print(f'{"Nome":<25}{"Idade":<5}{"Salário":>12}{"Sexo":>8}{"Estado Civil":>15}')
 print('-'*65)
