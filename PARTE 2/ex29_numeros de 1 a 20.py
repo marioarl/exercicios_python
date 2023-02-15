7742e61db2a2bfb6c230aa8055a9fc3b7b5d52be
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-15 18:09:06 -0300

    Update

--- PARTE 2/ex29_numeros de 1 a 20.py
+++ PARTE 2/ex29_numeros de 1 a 20.py
@@ -2,11 +2,12 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro
 '''
-
+from colorama import init, Fore
+init(autoreset=True)
 print('Os numeros de 1 a 20 um abaixo do outro:')
 for n in range(1,21):
     print(n)
 
-print('Agora um ao lado do outro:')
+print(Fore.CYAN + 'Agora um ao lado do outro:')
 for n in range(1,21):
-    print(f'{n} ', end="")
+    print(f'{Fore.GREEN}{n} ', end="")
