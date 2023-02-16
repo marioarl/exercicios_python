c51d9c90fa05131fb3da5deab561780f54bbf005
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-16 10:11:47 -0300

    Update

--- PARTE 2/ex35_media de alunos por turma.py
+++ PARTE 2/ex35_media de alunos por turma.py
@@ -4,6 +4,8 @@ Faça um programa que calcule o número médio de alunos por turma. Para isto, p
 quantidade de alunos para cada turma. 
 As turmas não podem ter mais de 40 alunos.
 '''
+from colorama import init, Fore
+init(autoreset=True)
 media = somaQtd = 0
 qtdTurmas = int(input('Qual a quantidade de turmas? '))
 
@@ -12,6 +14,6 @@ for t in range(1, qtdTurmas+1):
         qtdAlunos = int(input(f'  - Quantidade de alunos da TURMA {t}: '))
         if qtdAlunos <=40:
             break
-        print(f'\033[31mA turma {t} não pode ter mais de 40 alunos!\033[m')
+        print(f'{Fore.RED}A turma {t} não pode ter mais de 40 alunos!')
     somaQtd += qtdAlunos
 print(f'Numero médio de alunos por turma:  {somaQtd/ qtdTurmas:.1f}')
