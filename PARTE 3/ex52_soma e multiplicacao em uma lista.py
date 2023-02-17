3c01b504a1a976eb06e4036d32629f6679a04248
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-17 10:36:15 -0300

    Criacao ex52

---
+++ PARTE 3/ex52_soma e multiplicacao em uma lista.py
@@ -0,0 +1,19 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial
+
+Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+'''
+from os import system
+system('cls')
+soma = 0
+mult = 1
+
+numeros = []
+for n in range(1,6):
+    num = int(input(f'{n}o. numero: '))
+    soma += num
+    mult *= num
+    numeros.append(num)
+print(f'Os numeros digitados foram: {numeros}')
+print(f'Soma dos numeros...........: {soma}')
+print(f'Multiplicação dos numeros..: {mult}')
