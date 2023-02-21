953570549b83a54ef425885028972a2fe187b898
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-21 19:36:02 -0300

    Update

--- PARTE 1/ex01_raio de um circulo.py
+++ PARTE 1/ex01_raio de um circulo.py
@@ -2,7 +2,9 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um programa que peça o raio de um circulo, calcule e mostre sua área
 '''
+from colorama import Fore, init
+init(autoreset=True)
 from math import pi
 r = float(input('Digite o raio do circulo: '))
 area = pi * (r**2)
-print(f'A area do circulo de raio {r} é igual {area:.2f}')
+print(f'A area do circulo de raio {r} é igual {Fore.RED}{area:.2f}')
