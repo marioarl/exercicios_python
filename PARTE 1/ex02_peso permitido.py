c278ac8ba742d7d12329d48db233e4dc088f4237
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-21 19:43:43 -0300

    Update

--- PARTE 1/ex02_peso permitido.py
+++ PARTE 1/ex02_peso permitido.py
@@ -7,17 +7,20 @@ Joao precisa que voce faça um programa que leia a variavel peso(peso dos peixes
 Gravar na variavel excesso a quantidade de quilos alem do limite e na variavel multa o valor da multa 
 que o Joao devera pagar. Imprima os dados do programa com as mensagens adequadas.
 '''
-
-print('*'*30)
-print('CALCULO DE EXCESSO DE PESO'.center(30))
-print('*'*30)
+from os import system
+from colorama import Fore, Style, init
+init(autoreset=True)
+system('cls')
+print(Fore.YELLOW + '*'*30)
+print(Fore.YELLOW + 'CALCULO DE EXCESSO DE PESO'.center(30))
+print(Fore.YELLOW + '*'*30)
 peso = float(input('Peso dos peixes(Kg): '))
 
 if peso > 50:
     excesso = peso - 50
     multa = 4 * excesso
-    print(f'\033[31mVoce está levando {excesso}Kg acima do peso permitido(50Kg)!\033[m')
-    print(f'\033[31mVoce terá que pagar uma multa de R${multa:.2f}\033[m')
+    print(f'{Fore.RED}Voce está levando {excesso}Kg acima do peso permitido(50Kg)!')
+    print(f'{Fore.RED}Voce terá que pagar uma multa de R${multa:.2f}')
 else:
     print('Voce está levando a quantidade de acordo com o peso permitido')
-print('Tenha um bom dia!')
+print(Fore.GREEN + Style.BRIGHT + 'Tenha um bom dia!')
