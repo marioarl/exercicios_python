219b219ac8e57256aef82c5aecddc14aa8123aac
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-21 19:32:01 -0300

    Update

--- PARTE 2/ex25_nota invalida.py
+++ PARTE 2/ex25_nota invalida.py
@@ -3,11 +3,12 @@ Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
 e continue pedindo até que o usuário informe um valor válido.
 '''
-
+from colorama import Fore, init
+init(autoreset=True)
 while True:
     nota = int(input('Digite uma nota entre 0 e 10: '))
     if nota <0 or nota >10:
-        print('Valor Invalido!! ', end="")
+        print(Fore.RED + 'Valor Invalido!! ', end="")
     
     if nota >0 and nota <=10:
         break
