2beebd7aa9f2bbdbdd57890d525fd252496fd8e8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-21 19:20:57 -0300

    Update

--- PARTE 3/ex52_soma e multiplicacao em uma lista.py
+++ PARTE 3/ex52_soma e multiplicacao em uma lista.py
@@ -3,6 +3,8 @@ Credito : https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
 '''
+from colorama import Fore, init
+init(autoreset=True)
 from os import system
 system('cls')
 soma = 0
@@ -14,6 +16,6 @@ for n in range(1,6):
     soma += num
     mult *= num
     numeros.append(num)
-print(f'Os numeros digitados foram: {numeros}')
-print(f'Soma dos numeros...........: {soma}')
-print(f'Multiplicação dos numeros..: {mult}')
+print(f'Os numeros digitados foram: {Fore.RED}{numeros}')
+print(f'Soma dos numeros...........: {Fore.GREEN}{soma}')
+print(f'Multiplicação dos numeros..: {Fore.CYAN}{mult}')
