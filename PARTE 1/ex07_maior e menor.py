17db1239b518c30578cd1b9ef913ffc3ad35576e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-22 15:17:14 -0300

    Update

--- PARTE 1/ex07_maior e menor.py
+++ PARTE 1/ex07_maior e menor.py
@@ -2,6 +2,8 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um programa que peça 5 numeros e imprima qual é o maior e o menor
 '''
+from colorama import Fore, init
+init(autoreset=True)
 
 # Opção 1 => Utilizando as funcoes max(para achar o maior numero) e min(para o menor numero) e
 # colocando os numeros em uma lista
@@ -9,8 +11,8 @@ numeros = []
 for n in range(1,6):
     numeros.append(int(input(f'Digite o {n}o. numero: ')))
 print(f'Os numeros digitados foram: {numeros}')
-print(f'Numero maior: {max(numeros)}')
-print(f'Numero menor: {min(numeros)}')
+print(f'{Fore.GREEN}Numero maior: \033[m{max(numeros)}')
+print(f'{Fore.RED}Numero menor: \033[m{min(numeros)}')
 
 #opção 2 => Utilizando variaveis maior e menor
 maior = menor = 0
