bfbccca9f2cd89fc4d66941cace60d8bfabcb2af
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-22 15:09:58 -0300

    Update

--- PARTE 1/ex05_numero real e inteiro.py
+++ PARTE 1/ex05_numero real e inteiro.py
@@ -5,27 +5,28 @@ a) o produto do dobro do primeiro com a metade do segundo
 b) a soma do triplo do primeiro com o terceiro
 c) o terceiro elevado ao cubo
 '''
-
+from colorama import Fore, init
+init(autoreset=True)
 while True:
     try:
         numInteiro1= int(input('Digite o 1o. numero inteiro:'))
         break
     except ValueError:
-        print('\033[31mERRO!Digite um numero inteiro\033[m')
+        print(Fore.RED + 'ERRO!Digite um numero inteiro')
 
 while True:
     try:
         numInteiro2= int(input('Digite o 2o. numero inteiro:'))
         break
     except ValueError:
-        print('\033[31mERRO!Digite um numero inteiro\033[m')
+        print(Fore.RED + 'ERRO!Digite um numero inteiro')
 
 while True:
     try:
         numReal= float(input('Digite um numero Real:'))
         break
     except ValueError:
-        print('\033[31mERRO!Digite um numero inteiro\033[m')
+        print(Fore.RED + 'ERRO!Digite um numero inteiro')
 
 print(f'A) O produto do dobro do primeiro com a metade do segundo: {(numInteiro1*2) * numInteiro2 / 2}')
 print(f'B) A soma do triplo do primeiro com o terceiro: {(numInteiro1*3) + numReal}')
