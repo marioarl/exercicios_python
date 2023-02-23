ef8bd57cb23c3187e438482955724cfa0937030c
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-23 10:05:46 -0300

    Criacao ex53

---
+++ PARTE 3/ex53_soma dos quadrados.py
@@ -0,0 +1,19 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex listas 9
+
+Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
+'''
+from os import system
+system('cls')
+
+a = []
+somaQuadrado = 0
+
+for n in range(1,11):
+    numero = int(input(f'Digite o {n}o. numero: '))
+    a.append(numero)
+    somaQuadrado += numero ** 2
+
+print(f'Os numeros digitados foram: {a}')
+print(f'A soma dos quadrados dos elementos do vetor é: {somaQuadrado}')
+
