626f29f7d23e25506758e46c76b20bfde5101874
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-26 12:12:46 -0300

    Update

--- PARTE 1/ex11_media do aluno v2.py
+++ PARTE 1/ex11_media do aluno v2.py
@@ -8,7 +8,7 @@ A mensagem "Aprovado com Distinção", se a média for igual a dez.
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota:  '))
 media = (n1+n2) / 2
-print(f'A media das notas acima é: {media:.1f}')
+print(f'A média das notas acima é: {media:.1f}')
 print('O aluno está ', end="")
 if media == 10:
     print('APROVADO COM DISTINÇÃO')
