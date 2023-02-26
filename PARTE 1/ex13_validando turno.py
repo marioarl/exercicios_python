b5221d6d89f88daafbc73444751884f81f6cc061
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-26 12:18:22 -0300

    Update

--- PARTE 1/ex13_validando turno.py
+++ PARTE 1/ex13_validando turno.py
@@ -2,6 +2,8 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 '''
+from os import system
+system('cls')
 print('Em qual turno voce estuda?')
 turno = str(input('M-matutino | V-vespertino | N-noturno: ')).strip().upper()[0]
 if turno in "M":
