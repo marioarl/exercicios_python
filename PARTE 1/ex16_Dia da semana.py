7a1080ec0f3478c5e722d6bc6b01cbe64c1c6bf8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-26 12:25:31 -0300

    Update

--- PARTE 1/ex16_Dia da semana.py
+++ PARTE 1/ex16_Dia da semana.py
@@ -2,10 +2,12 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
 '''
+from colorama import Fore, init
+init(autoreset=True)
 dias = ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
 
 n = int(input('Digite um numero correspondente ao dias da semana: '))
-if n >=0 or n <=7:
-    print(f'O dia da semana digitado foi {dias[n-1]}')
+if n >=0 and n <=7:
+    print(f'O dia da semana digitado foi {Fore.GREEN}{dias[n-1]}')
 else:
-    print('Valor inválido')
+    print(Fore.RED + 'Valor inválido')
