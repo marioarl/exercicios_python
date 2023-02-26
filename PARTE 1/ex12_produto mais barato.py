b9d608c8fb7f48a60f6acc26c2eaf35c41f02191
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-26 12:17:23 -0300

    Update

--- PARTE 1/ex12_produto mais barato.py
+++ PARTE 1/ex12_produto mais barato.py
@@ -2,14 +2,16 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 '''
-prodA = float(input('Preço do Produto A: R$ '))
-prodB = float(input('Preço do Produto B: R$ '))
-prodC = float(input('Preço do Produto C: R$ '))
+from colorama import Fore, init
+init(autoreset=True)
+prodA = float(input(Fore.YELLOW + 'Preço do Produto A: R$ ' + Fore.RESET))
+prodB = float(input(Fore.RED + 'Preço do Produto B: R$ ' + Fore.RESET))
+prodC = float(input(Fore.GREEN + 'Preço do Produto C: R$ ' + Fore.RESET))
 
 print('O produto mais barato é o ', end="")
 if prodA < prodB < prodC:
-    print('PRODUTO A')
+    print(Fore.YELLOW + 'PRODUTO A')
 elif prodB < prodC and prodB < prodA:
-    print('PRODUTO B')
+    print(Fore.RED + 'PRODUTO B')
 elif prodC < prodA and prodC < prodB:
-    print('PRODUTO C')
+    print(Fore.GREEN + 'PRODUTO C')
