9c10a9592f405a8b9beefc841054bdc3f6157de8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-26 12:11:27 -0300

    Update

--- PARTE 3/ex51_media de alunos em uma lista.py
+++ PARTE 3/ex51_media de alunos em uma lista.py
@@ -4,14 +4,16 @@ Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 '''
 from os import system
+from colorama import Fore,init
+init(autoreset=True)
 system('cls')
 medias = []
 cont = 0
 for a in range(1,11):
     media = somaNota = 0
-    print(f'Digite as 4 notas do aluno {a}')
+    print(f'{Fore.YELLOW}Digite as 4 notas do aluno {a}')
     for n in range(1,5):
-        nota = float(input(f' - Digite a {n}a. nota: '))
+        nota = float(input(f'{Fore.GREEN} - Digite a {n}a. nota: '))
         somaNota += nota
     media = somaNota / 4
     medias.append(media)
