2f18f5bebeed226ee56b35331c9339fe2963df75
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-27 16:11:40 -0300

    Update

--- PARTE 1/ex17_media do aluno v3.py
+++ PARTE 1/ex17_media do aluno v3.py
@@ -9,16 +9,17 @@ Faça um programa que lê as duas notas parciais obtidas por um aluno numa disci
   Entre 4.0 e zero        E
 O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E
 '''
-
+from colorama import Fore, init
+init(autoreset=True)
 n1 = float(input('Digita a 1a. nota: '))
 n2 = float(input('Digita a 2a. nota: '))
 media = (n1 + n2) / 2
 if media >= 9:
-    conceito = "A"
+    conceito = Fore.GREEN + "A"
 elif media >=7.5:
-    conceito = "B"
+    conceito = Fore.BLUE + "B"
 elif media >=6:
-    conceito = "C"
+    conceito = Fore.YELLOW + "C"
 elif media >= 4:
     conceito = "D"
 else:
