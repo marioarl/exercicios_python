c1eb751d1e7c63c200a56d0556cb8df7311ccac2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-27 16:24:46 -0300

    Update

--- PARTE 1/ex19_informacao de resultado.py
+++ PARTE 1/ex19_informacao de resultado.py
@@ -13,6 +13,7 @@ n2 = float(input('Digite o 2o. numero: '))
 res = 0
 print(Fore.BLUE + "1 \033[m- Somar\n2 - Subtrair\n3 - Dividir\n4 - Multiplicar")
 print(Fore.YELLOW + "2 \033[m - Subtrair")
+print(Fore.GREEN + "3 \033[m - Dividir")
 while True:
     op = str(input('Qual operação deseja realizar? ')).strip()[0]
     if op in "1234":
