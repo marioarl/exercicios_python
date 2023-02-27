779ecad71ab4ed0491ce09787d67160c7fdfe738
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-27 16:18:43 -0300

    Update

--- PARTE 1/ex18_par ou impar.py
+++ PARTE 1/ex18_par ou impar.py
@@ -2,10 +2,11 @@
 Credito : https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
 '''
-
+from colorama import  Fore, init
+init(autoreset=True)
 num = int(input('Digite um numero: '))
 print(f'O numero {num} digitado é ', end="")
 if num % 2 == 0:
-    print("PAR")
+    print(Fore.BLUE + "PAR")
 else:
-    print("IMPAR")
+    print(Fore.RED + "IMPAR")
