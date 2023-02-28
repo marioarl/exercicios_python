89c23e7fe93b586d71a9604385451426f1538106
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-28 09:51:40 -0300

    Update ex20_culpado ou inocente.py

--- PARTE 1/ex20_culpado ou inocente.py
+++ PARTE 1/ex20_culpado ou inocente.py
@@ -9,6 +9,7 @@ Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As pergu
 O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 '''
 from os import system
+system('cls')
 cont = 0
 p1 = str(input('1) Telefonou para a vitima?   [S/N]: ')).strip().upper()[0]
 if p1 in "S":
