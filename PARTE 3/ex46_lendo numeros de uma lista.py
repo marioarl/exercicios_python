e61e13e70bb145c5cbeb50fcaef9570d902fe393
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-04 07:09:17 -0300

    Update ex46_lendo numeros de uma lista.py

--- PARTE 3/ex46_lendo numeros de uma lista.py
+++ PARTE 3/ex46_lendo numeros de uma lista.py
@@ -3,7 +3,8 @@ Credito : https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
 '''
-
+from os import system
+system('cls')
 listNum = []
 
 for n in range(1,6):
