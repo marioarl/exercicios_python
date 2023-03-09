cb58eeddcb602a5869f376398d783ea0879b0bab
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-09 10:31:28 -0300

    Update ex09_verificar o sexo digitado.py

--- PARTE 1/ex09_verificar o sexo digitado.py
+++ PARTE 1/ex09_verificar o sexo digitado.py
@@ -9,6 +9,6 @@ sexo = str(input('Digite o sexo [M/F]: ')).strip().upper()[0]
 if sexo in "M":
     print(Fore.BLUE + 'Voce digitou o sexo Masculino')
 elif sexo in "F":
-    print(f'Voce digitou o sexo Feminino')
+    print(Fore.MAGENTA + 'Voce digitou o sexo Feminino')
 else:
     print('Sexo Inválido')
