2e78c29b2e96bd77feb2fe396566dce512f1f68f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-10 09:27:59 -0300

    Criacao ex62

---
+++ PARTE 3/ex62_lancamento de dados.py
@@ -0,0 +1,31 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex listas 24
+
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um vetor . Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, simulando os lançamentos dos dados.
+
+'''
+from os import system
+from random import randint
+from collections import Counter
+from colorama import Fore, init
+init(autoreset=True)
+
+system('cls')
+def aleatorio(ini, fim):
+    x = randint(ini,fim)
+    return x
+
+resultados = []
+
+for i in range(100):
+    resultados.append(aleatorio(1,6))
+
+resultados.sort()
+
+contagem = Counter(resultados)
+
+for n, q in contagem.items():
+    print(f'O numero {n} foi sorteado {q} vezes.')
+
+
+
