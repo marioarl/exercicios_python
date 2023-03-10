2ee8ebae990f5466e476cb5c55d9d5957b2f5703
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-10 09:57:57 -0300

    Criacao ex65

---
+++ PARTE 4/ex65_func positivo e negativo.py
@@ -0,0 +1,16 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex funcoes 4
+
+Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere P se seu argumento for positivo, e N, se seu argumento for zero ou negativo.
+'''
+from os import system
+system('cls')
+
+def pos_neg(n):
+    if n % 2 == 0:
+        print('P')
+    else:
+        print('N')
+
+
+pos_neg(8)
