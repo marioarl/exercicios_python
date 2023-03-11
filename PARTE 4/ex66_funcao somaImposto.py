ac0437fbf7256d6cb912ebc98f934d20f95eb3e3
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-11 10:50:59 -0300

    Criacao ex66

---
+++ PARTE 4/ex66_funcao somaImposto.py
@@ -0,0 +1,17 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex funcoes 5
+
+Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
+'''
+from os import system
+system('cls')
+
+def somaImposto(taxaImposto, custo):
+    imposto = custo * (taxaImposto/100)
+    valor_final = custo + imposto
+    return valor_final
+
+
+preco = float(input('Qual o valor do produto? R$ '))
+imposto = float(input('Porcentagem do imposto: '))
+print(f'O valor final do produto com imposto é R$ {somaImposto(imposto,preco):.2f}')
