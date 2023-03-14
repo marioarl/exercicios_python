a47d3bbeec45418479cf5f3005281fd02cf7c1aa
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-14 10:17:54 -0300

    Criacao ex70

---
+++ PARTE 4/ex70_numero reverso.py
@@ -0,0 +1,15 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex funcoes 9
+
+Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.   
+'''
+from os import system
+system('cls')
+
+def inverso(num):
+    x = str(num)
+    rev = x[::-1]
+    return rev
+
+n = int(input('Digite um numero inteiro: '))
+print(f'O reverso do numero digitado {n} é {inverso(n)}')
