ecae2c07c901a5ac564b219874dfc338142d9e35
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-17 10:02:28 -0300

    Update ex74_retangulo.py

--- PARTE 4/ex74_retangulo.py
+++ PARTE 4/ex74_retangulo.py
@@ -10,7 +10,8 @@ init(autoreset=True)
 system('cls')
 
 
-def desenha_retangulo(linhas=1, colunas=1):
+def desenha_retangulo(linhas=1, colunas=1, cor=""):
+    cores = [Fore.RED, Fore.BLACK, Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.MAGENTA, Fore.WHITE, Fore.YELLOW]
     if linhas < 1:
         linhas = 1
     elif linhas > 20:
@@ -23,12 +24,15 @@ def desenha_retangulo(linhas=1, colunas=1):
         colunas = 20
         print(Fore.RED + 'Desculpe, infelizmente só posso desenhar no máximo 20 colunas')
         sleep(2)
-    print('+' + '-'*(colunas-2) + '+')
+    print(cores[cor-1] + '+' + '-'*(colunas-2) + '+')
     for i in range(linhas-2):
-        print('|' + ' '*(colunas-2) + '|')
-    print('+' + '-'*(colunas-2) + '+')
+        print(cores[cor-1] + '|' + ' '*(colunas-2) + '|')
+    print(cores[cor-1] + '+' + '-'*(colunas-2) + '+')
 
 print('===== DESENHAR UM RETANGULO =====')
+
 l = int(input('Digite o numero de linhas: '))
 c = int(input('Digite o numero de colunas:'))
-desenha_retangulo(l,c)
+print('Escolha a cor:\n[ 1 ] - VERMELHO\n[ 2 ] - PRETO\n[ 3 ] - AZUL\n[ 4 ] - CIANO\n[ 5 ] - VERDE\n[ 6 ] - VIOLETA\n[ 7 ] - BRANCO\n[ 8 ] - AMARELO')
+cor = int(input('Escolha a cor do retangulo: '))
+desenha_retangulo(l,c, cor)
