88a2d38e23d189bc607938df0801eaf7a619dcf5
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-19 10:24:01 -0300

    Update ex75_tamanho da string.py

--- PARTE 5/ex75_tamanho da string.py
+++ PARTE 5/ex75_tamanho da string.py
@@ -21,7 +21,7 @@ system('cls')
 string1 = str(input('Digite algo: ')).strip()
 string2 = str(input('Digite algo: ')).strip()
 
-print('\n\nCompara duas strings')
+print(Fore.GREEN + '\n\nCompara duas strings')
 print(f'String 1: {string1}')
 print(f'String 2: {string2}')
 print(f'Tamanho de {string1}: {len(string1)}')
