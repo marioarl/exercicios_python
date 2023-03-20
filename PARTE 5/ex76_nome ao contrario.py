f95530769ccb0c3cf2a46cd0da3e742921bf4c70
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-20 07:47:27 -0300

    Update ex76_nome ao contrario.py

--- PARTE 5/ex76_nome ao contrario.py
+++ PARTE 5/ex76_nome ao contrario.py
@@ -8,6 +8,7 @@ Dica: lembre que ao informar o nome o usuário pode digitar letras maiúsculas o
 from os import system
 system('cls')
 
+print('===== NOME AO CONTRARIO =====')
 nome = str(input('Digite seu nome: ')).strip()
 print(nome.upper()[::-1])
 
