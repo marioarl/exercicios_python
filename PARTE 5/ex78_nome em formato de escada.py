aa6c68cff97f5473d991f5a8e88a4998d5cc4d1d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-21 11:24:56 -0300

    Criacao ex78.py

---
+++ PARTE 5/ex78_nome em formato de escada.py
@@ -0,0 +1,21 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex Com Strings 4
+
+Nome na vertical em escada. Modifique o programa anterior de forma a mostrar o nome em formato de escada.
+
+F
+FU
+FUL
+FULA
+FULAN
+FULANO
+'''
+from os import system
+system('cls')
+
+nome = str(input('Digite seu nome:'))
+tamanho = len(nome)
+for p in range(len(nome)):
+    print(nome[0:p+1])
+
+
