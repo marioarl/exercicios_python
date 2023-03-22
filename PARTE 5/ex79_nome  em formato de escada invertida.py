e63e8366478ed9194f374e37bfc647162cbfedb9
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-22 14:44:08 -0300

    Update ex79_nome  em formato de escada invertida.py

--- PARTE 5/ex79_nome  em formato de escada invertida.py
+++ PARTE 5/ex79_nome  em formato de escada invertida.py
@@ -16,6 +16,7 @@ from colorama import Fore, init
 init(autoreset=True)
 system('cls')
 cores = [Fore.RED, Fore.BLUE, Fore.GREEN, Fore.CYAN, Fore.YELLOW]
+num = randint(0,4)
 nome = str(input('Digite seu nome:'))
 
 for i in range(len(nome)):
