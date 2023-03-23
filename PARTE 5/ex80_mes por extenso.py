d5bd64a5f54521e7e6b010eceb6ab6d9542912d3
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-23 10:00:26 -0300

    Create ex80_mes por extenso.py

---
+++ PARTE 5/ex80_mes por extenso.py
@@ -0,0 +1,21 @@
+'''
+Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex Com Strings 6
+
+Data por extenso. Faça um programa que solicite a data de nascimento (dd/mm/aaaa) do usuário e imprima a data com o nome do mês por extenso.
+
+Data de Nascimento: 29/10/1973
+Você nasceu em  29 de Outubro de 1973.
+'''
+from os import system
+system('cls')
+
+dia = int(input('Dia: '))
+mes = int(input('Mes: '))
+ano = int(input('Ano: '))
+
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+print(f'Voce nasceu em {dia} de {meses[mes-1]} de {ano}')
+    
+
+
