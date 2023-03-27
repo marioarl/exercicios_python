f8c63ab0d18438d8b57e8e7dcbd3f25acdbbb4a7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-27 11:20:28 -0300

    Update ex09_verificar o sexo digitado.py

--- PARTE 1/ex09_verificar o sexo digitado.py
+++ PARTE 1/ex09_verificar o sexo digitado.py
@@ -1,6 +1,7 @@
 '''
 Credito : https://wiki.python.org.br/EstruturaSequencial
-Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 '''
 from colorama import Fore, init
 init(autoreset=True)
