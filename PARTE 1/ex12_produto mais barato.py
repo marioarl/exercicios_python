5a51735baa7cc0a90b214c6466f3aacbfb6d46e0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:19:15 -0300

    Update ex12_produto mais barato.py

--- PARTE 1/ex12_produto mais barato.py
+++ PARTE 1/ex12_produto mais barato.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex8
 Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 '''
 from colorama import Fore, init
