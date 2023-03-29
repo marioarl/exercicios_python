b04ccd1c2adb2369e71c709922471e48ec3f5415
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:21:56 -0300

    Update ex15_folha de pagamento.py

--- PARTE 1/ex15_folha de pagamento.py
+++ PARTE 1/ex15_folha de pagamento.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex12
 Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, 
 que depende do salário bruto (conforme tabela abaixo) e que o FGTS corresponde a 11% 
 do Salário Bruto, mas não é descontado (é a empresa que deposita). 
