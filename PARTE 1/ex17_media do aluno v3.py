04cd85262e1cac02e1738175d77eb81b7ff2418e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:24:08 -0300

    Update ex17_media do aluno v3.py

--- PARTE 1/ex17_media do aluno v3.py
+++ PARTE 1/ex17_media do aluno v3.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex14
 Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
   Média de Aproveitamento  Conceito
   Entre 9.0 e 10.0        A
