c7dde909e27e38d76ec2f67c8f6c8b43af98a1f4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:25:34 -0300

    Update ex19_informacao de resultado.py

--- PARTE 1/ex19_informacao de resultado.py
+++ PARTE 1/ex19_informacao de resultado.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex24
 Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
  - par ou ímpar;
  - positivo ou negativo;
