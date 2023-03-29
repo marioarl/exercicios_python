50852c2dda720166a8a789a1f9068d29b1dddcfa
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:12:07 -0300

    Update ex11_media do aluno v2.py

--- PARTE 1/ex11_media do aluno v2.py
+++ PARTE 1/ex11_media do aluno v2.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex5
 Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
 A mensagem "Aprovado", se a média alcançada for maior ou igual a 7;
 A mensagem "Reprovado", se a média for menor do que 7;
