db7aba6b7b2922cb9c500f1cbcb11f0e0e038667
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-29 09:24:56 -0300

    Update ex18_par ou impar.py

--- PARTE 1/ex18_par ou impar.py
+++ PARTE 1/ex18_par ou impar.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex22
 Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
 '''
 from colorama import  Fore, init
