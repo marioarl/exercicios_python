6165b1e8c8927d45486e9f12f8074dbcaa79e766
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-30 11:05:54 -0300

    Update ex25_nota invalida.py

--- PARTE 2/ex25_nota invalida.py
+++ PARTE 2/ex25_nota invalida.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex1
 Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
 e continue pedindo até que o usuário informe um valor válido.
 '''
