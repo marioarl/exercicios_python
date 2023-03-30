33a26ebe3b65891436322b0ae750017caf7a4998
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-30 11:09:11 -0300

    Update ex29_numeros de 1 a 20.py

--- PARTE 2/ex29_numeros de 1 a 20.py
+++ PARTE 2/ex29_numeros de 1 a 20.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex6
 Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro
 '''
 from colorama import init, Fore
