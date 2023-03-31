1d183dc4061d16f76014f927625b2a3e4abd717b
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-31 09:05:02 -0300

    Update ex39_tabuada personalizada.py

--- PARTE 2/ex39_tabuada personalizada.py
+++ PARTE 2/ex39_tabuada personalizada.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex36
 
 Desenvolva um programa que faça a tabuada de um número qualquer inteiro que será digitado pelo usuário, mas a tabuada não deve necessariamente iniciar em 1 e terminar em 10, o valor inicial e final devem ser informados também pelo usuário, conforme exemplo abaixo:
 Montar a tabuada de: 5
