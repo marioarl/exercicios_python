87fe212ef4e64724fbc8ba507bc03145510064dc
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-31 09:04:13 -0300

    Update ex38_media de temperaturas.py

--- PARTE 2/ex38_media de temperaturas.py
+++ PARTE 2/ex38_media de temperaturas.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex33
 O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia um conjunto indeterminado de temperaturas, e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
 '''
 from colorama import init, Fore
