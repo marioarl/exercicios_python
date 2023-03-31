806c7337543a79554326f3a94af6e05116d0dc7b
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-31 09:05:33 -0300

    Update ex40_programa de clientes de academia.py

--- PARTE 2/ex40_programa de clientes de academia.py
+++ PARTE 2/ex40_programa de clientes de academia.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex37
 
 Uma academia deseja fazer um senso entre seus clientes para descobrir o mais alto, o mais baixo, a mais gordo e o mais magro, para isto você deve fazer um programa que pergunte a cada um dos clientes da academia seu código, sua altura e seu peso. O final da digitação de dados deve ser dada quando o usuário digitar 0 (zero) no campo código. 
 Ao encerrar, o programa também deve informar os códigos e valores do cliente mais alto, do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
