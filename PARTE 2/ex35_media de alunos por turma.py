ffe72534c33a3df813f19e586ace80501dba6326
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-31 09:02:16 -0300

    Update ex35_media de alunos por turma.py

--- PARTE 2/ex35_media de alunos por turma.py
+++ PARTE 2/ex35_media de alunos por turma.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial
+Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex27
 Faça um programa que calcule o número médio de alunos por turma. Para isto, peça a quantidade de turmas e a 
 quantidade de alunos para cada turma. 
 As turmas não podem ter mais de 40 alunos.
