fc36f4d9a845ea9a16817a24ffd17d6fea034dea
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-02 12:05:34 -0300

    Update ex57_enquete de futebol.py

--- PARTE 3/ex57_enquete de futebol.py
+++ PARTE 3/ex57_enquete de futebol.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex listas 18
+Credito : https://wiki.python.org.br/ExerciciosListas ref: Ex listas 18
 
 Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para saber qual o melhor jogador após cada jogo. 
 Para isto, faz-se necessário o desenvolvimento de um programa, que será utilizado pelas telefonistas, para a computação dos votos. Sua equipe foi contratada para desenvolver este programa, utilizando a linguagem Python. 
