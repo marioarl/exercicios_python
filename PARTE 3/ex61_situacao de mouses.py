492d362c1d28399593ee828dc782db1b125f9ed5
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-03 09:47:10 -0300

    Update ex61_situacao de mouses.py

--- PARTE 3/ex61_situacao de mouses.py
+++ PARTE 3/ex61_situacao de mouses.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex listas 22
+Credito : https://wiki.python.org.br/ExerciciosListas ref: Ex listas 22
 
 Sua organização acaba de contratar um estagiário para trabalhar no Suporte de Informática, com a intenção de fazer um levantamento nas sucatas encontradas nesta área. A primeira tarefa dele é testar todos os cerca de 200 mouses que se encontram lá, testando e anotando o estado de cada um deles, para verificar o que se pode aproveitar deles.
 Foi requisitado que você desenvolva um programa para registrar este levantamento. O programa deverá receber um número indeterminado de entradas, cada uma contendo: um número de identificação do mouse o tipo de defeito:
