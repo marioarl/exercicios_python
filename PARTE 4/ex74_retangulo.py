b55b77726871b4eac1e3f54752174d60c014ea6d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-04 07:41:05 -0300

    Update ex74_retangulo.py

--- PARTE 4/ex74_retangulo.py
+++ PARTE 4/ex74_retangulo.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex funcoes 13
+Credito : https://wiki.python.org.br/ExerciciosFuncoes ref: Ex funcoes 13
 
 Desenha moldura. Construa uma função que desenhe um retângulo usando os caracteres + , - e | . Esta função deve receber dois parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo igual a 1 e o valor máximo é 20. Se valores fora da faixa forem informados, eles devem ser modificados para valores dentro da faixa de forma elegante.
 '''
