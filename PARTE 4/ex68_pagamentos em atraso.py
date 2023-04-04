4813ab1ae5ab54ae3f4dbf724eb729de234a0df5
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-04 07:38:16 -0300

    Update ex68_pagamentos em atraso.py

--- PARTE 4/ex68_pagamentos em atraso.py
+++ PARTE 4/ex68_pagamentos em atraso.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex funcoes 7
+Credito : https://wiki.python.org.br/ExerciciosFuncoes ref: Ex funcoes 7
 
 Faça um programa que use a função valorPagamento para determinar o valor a ser pago por uma prestação de uma conta. O programa deverá solicitar ao usuário o valor da prestação e o número de dias em atraso e passar estes valores para a função valorPagamento, que calculará o valor a ser pago e devolverá este valor ao programa que a chamou. O programa deverá então exibir o valor a ser pago na tela. 
 Após a execução, o programa deverá voltar a pedir outro valor de prestação e assim continuar até que seja informado um valor igual a zero para a prestação. Neste momento o programa deverá ser encerrado, exibindo o relatório do dia, que conterá a quantidade e o valor total de prestações pagas no dia. 
