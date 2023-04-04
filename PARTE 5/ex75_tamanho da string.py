ccfba3f475c54b34ee906fa80affed0c5667669e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-04 07:42:14 -0300

    Update ex75_tamanho da string.py

--- PARTE 5/ex75_tamanho da string.py
+++ PARTE 5/ex75_tamanho da string.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex Com Strings 1
+Credito : https://wiki.python.org.br/ExerciciosComStrings ref: Ex Com Strings 1
 
 Tamanho de strings. 
 Faça um programa que leia 2 strings e informe o conteúdo delas seguido do seu comprimento. Informe também se as duas strings possuem o mesmo comprimento e são iguais ou diferentes no conteúdo.
