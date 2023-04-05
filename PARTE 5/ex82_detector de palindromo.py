51e3dd494ce3afd353dca230ad9268efa12f5d92
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-05 09:19:24 -0300

    Update ex82_detector de palindromo.py

--- PARTE 5/ex82_detector de palindromo.py
+++ PARTE 5/ex82_detector de palindromo.py
@@ -1,5 +1,5 @@
 '''
-Credito : https://wiki.python.org.br/EstruturaSequencial ref: Ex Com Strings 8
+Credito : https://wiki.python.org.br/ExerciciosComStrings ref: Ex Com Strings 8
 
 Palíndromo. Um palíndromo é uma seqüência de caracteres cuja leitura é idêntica se feita da direita para esquerda ou vice versa. Por exemplo: OSSO e OVO são palíndromos. Em textos mais complexos os espaços e pontuação são ignorados. A frase SUBI NO ONIBUS é o exemplo de uma frase palíndroma onde os espaços foram ignorados. Faça um programa que leia uma seqüência de caracteres, mostre e diga se é um palíndromo ou não.
 '''
