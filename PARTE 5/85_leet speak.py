d547dd0be8384f4704d96408039ed69a6f83e84f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-07 11:42:52 -0300

    Update 85_leet speak.py

--- PARTE 5/85_leet speak.py
+++ PARTE 5/85_leet speak.py
@@ -5,6 +5,7 @@ Leet speak generator.
 Leet é uma forma de se escrever o alfabeto latino usando outros símbolos em lugar das letras, como números por exemplo. A própria palavra leet admite muitas variações, como l33t ou 1337. O uso do leet reflete uma subcultura relacionada ao mundo dos jogos de computador e internet, sendo muito usada para confundir os iniciantes e afirmar-se como parte de um grupo. Pesquise sobre as principais formas de traduzir as letras. Depois, faça um programa que peça uma texto e transforme-o para a grafia leet speak.
 '''
 from os import system
+from colorama import Fore, init
 
 system('cls')
 
@@ -14,7 +15,7 @@ def leetSpeak(frase):
         indice = letras.index(l)
         print(leet[indice], end="")
 
-        
+
 letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', ' ']
 leet = ['4','I3','[', ')', '3', '|=', '6', '#', '1', ',_|', '>|', '1', '[v]', '/v', '0', 'P', '9', 'I2', '5', '7', '(_)', '|/', 'vv', "><", 'j', '2', ' ']
 
