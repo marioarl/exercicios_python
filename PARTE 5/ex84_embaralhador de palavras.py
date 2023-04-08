05c3dde00263142985dabbfd6e77a2bb08f0d86a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-08 17:16:40 -0300

    Update ex84_embaralhador de palavras.py

--- PARTE 5/ex84_embaralhador de palavras.py
+++ PARTE 5/ex84_embaralhador de palavras.py
@@ -2,7 +2,8 @@
 Credito : https://wiki.python.org.br/ExerciciosComStrings ref: Ex Com Strings 13
 
 Jogo da palavra embaralhada. 
-Desenvolva um jogo em que o usuário tenha que adivinhar uma palavra que será mostrada com as letras embaralhadas. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente. O jogador terá seis tentativas para adivinhar a palavra. Ao final a palavra deve ser mostrada na tela, informando se o usuário ganhou ou perdeu o jogo.
+Desenvolva um jogo em que o usuário tenha que adivinhar uma palavra que será mostrada com as letras embaralhadas. O programa terá uma lista de palavras lidas de um arquivo texto e escolherá uma aleatoriamente. 
+O jogador terá seis tentativas para adivinhar a palavra. Ao final a palavra deve ser mostrada na tela, informando se o usuário ganhou ou perdeu o jogo.
 '''
 from os import system
 from random import shuffle, randint
