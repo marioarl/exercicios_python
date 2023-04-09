5689b14c26f1bed3c8a5dfc1eb9af7c15cdd4a07
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-09 10:45:48 -0300

    Update ex86_arquivos.py

--- PARTE 6/ex86_arquivos.py
+++ PARTE 6/ex86_arquivos.py
@@ -1,3 +1,8 @@
+#Criando o arquivo de entrada
+with open("entrada.txt", "w") as f:
+    f.write("200.135.80.9\n192.168.1.1\n8.35.67.74\n257.32.4.5\n85.345.1.2\n1.2.3.4\n9.8.234.5\n192.168.0.256")
+print("Arquivo criado com sucesso!")
+
 # Abrir o arquivo de entrada para leitura
 with open("entrada.txt", "r") as entrada:
     # Ler todas as linhas do arquivo de entrada
