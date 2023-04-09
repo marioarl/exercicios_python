7f18a278f49c7a2e121ac8ceb1d150ce92bfaf45
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-09 10:46:20 -0300

    Update ex86_arquivos.py

--- PARTE 6/ex86_arquivos.py
+++ PARTE 6/ex86_arquivos.py
@@ -1,3 +1,4 @@
+from colorama import Fore, init
 #Criando o arquivo de entrada
 with open("entrada.txt", "w") as f:
     f.write("200.135.80.9\n192.168.1.1\n8.35.67.74\n257.32.4.5\n85.345.1.2\n1.2.3.4\n9.8.234.5\n192.168.0.256")
