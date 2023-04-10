93b848e9cae438fca80c185c7b0478fa95a98a52
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-10 08:09:19 -0300

    Update ex87_classe bola.py

--- ex87_classe bola.py
+++ ex87_classe bola.py
@@ -6,7 +6,6 @@ Classe Bola: Crie uma classe que modele uma bola:
 Atributos: Cor, circunferência, material
 Métodos: trocaCor e mostraCor
 '''
-
 class Bola:
     def __init__(self, cor, circunferencia, material):
         self.cor = cor
