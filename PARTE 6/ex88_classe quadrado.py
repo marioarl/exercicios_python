6b61395958f26df53e78464a8b6e49f91ef3f238
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-11 10:18:41 -0300

    Update ex88_classe quadrado.py

--- PARTE 6/ex88_classe quadrado.py
+++ PARTE 6/ex88_classe quadrado.py
@@ -1,3 +1,11 @@
+'''
+Credito : https://wiki.python.org.br/ExerciciosClasses ref: Ex2
+
+Classe Quadrado: Crie uma classe que modele um quadrado:
+
+Atributos: Tamanho do lado
+Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+'''
 class Quadrado:
     def __init__(self, lado):
         self.lado = lado
