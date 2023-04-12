1e545511735444cf997691fd7b9df562538b4c68
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-12 16:09:24 -0300

    Criacao ex90

---
+++ PARTE 6/ex90_classe pessoa.py
@@ -0,0 +1,36 @@
+'''
+Credito : https://wiki.python.org.br/ExerciciosClasses ref: Ex4
+
+Classe Pessoa: Crie uma classe que modele uma pessoa:
+
+Atributos: nome, idade, peso e altura
+Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
+'''
+
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self):
+        self.idade += 1
+        if self.idade < 21:
+            self.altura += 0.5
+    
+    def engordar(self, kg):
+        self.peso += kg
+    
+    def emagrecer(self, kg):
+        self.peso -= kg
+    
+    def crescer(self, cm):
+        self.altura += cm
+
+
+pessoa1 = Pessoa('João', 18, 70, 170)
+pessoa1.envelhecer()
+pessoa1.engordar(5)
+pessoa1.crescer(2)
+print(pessoa1.nome, pessoa1.idade, pessoa1.peso, pessoa1.altura)
