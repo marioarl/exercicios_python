568dd657886b6ada09c4150e65c5e158e8bcd1e8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-13 11:40:22 -0300

    Criacao ex90

---
+++ PARTE 6/ex90_classe conta corrente.py
@@ -0,0 +1,29 @@
+'''
+Credito : https://wiki.python.org.br/ExerciciosArquivos ref: Ex 1
+
+Classe Conta Corrente: Crie uma classe para implementar uma conta corrente. 
+A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo. Os métodos são os seguintes: alterarNome, depósito e saque.
+No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
+'''
+class ContaCorrente:
+    def __init__(self,numero,nome,saldo=0):
+        self.num = numero
+        self.nome = nome
+        self.saldo = saldo
+    
+    def alterarNome(self,novo_nome):
+        self.nome = novo_nome
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if valor > self.saldo:
+            print("Saldo insuficiente!")
+        else:
+            self.saldo -= valor
+
+
+
+            
+
