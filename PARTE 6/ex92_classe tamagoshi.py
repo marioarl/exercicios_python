a4229bbc8f61527f720dbe7e793f2fcdf0f604fc
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-16 11:10:23 -0300

    Update ex92_classe tamagoshi.py

--- PARTE 6/ex92_classe tamagoshi.py
+++ PARTE 6/ex92_classe tamagoshi.py
@@ -1,11 +1,9 @@
 ''''
 Credito : https://wiki.python.org.br/ExerciciosClasses ref: Ex7
 
-Classe Bichinho Virtual:Crie uma classe que modele um Tamagoshi (Bichinho Eletrônico):
+Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
 
-Atributos: Nome, Fome, Saúde e Idade
-Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade 
-Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagoshi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
+Atributos: Nome, Fome, Saúde e Idade b. Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
 
 '''
 import os
@@ -18,7 +16,7 @@ else:
 class Tamagoshi:
     def __init__(self, nome, fome=0, saude=100, idade=0):
         self.nome = nome
-        self.fome = fome
+        self.fome = self.calcular_fome()
         self.saude = saude
         self.idade = idade
         self.humor = self.calcular_humor()
@@ -26,4 +24,4 @@ class Tamagoshi:
     def alterarNome(self,novo_nome):
         self.nome = novo_nome
 
-    
+tama = Tamagoshi("Yoshi")
