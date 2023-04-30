ab757e31ef600be7fef42f0455cdd61723aa12ce
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-30 09:32:03 -0300

    Update ex93_versao do Python.py

--- PARTE 6/ex93_versao do Python.py
+++ PARTE 6/ex93_versao do Python.py
@@ -4,3 +4,5 @@ Credito : https://www.huicode.com.br/2020/09/ler-um-valor-inteiro-aceitar-soment
 Faça um programa que informe a versão do Python que você está utilizando.
 '''
 
+import sys
+print(f"Versao do Python {sys.version}")
