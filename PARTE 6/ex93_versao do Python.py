beb2925a0e272f16460a9535d5b94e8e54e322e1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-01 18:51:37 -0300

    Update ex93_versao do Python.py

--- PARTE 6/ex93_versao do Python.py
+++ PARTE 6/ex93_versao do Python.py
@@ -6,6 +6,6 @@ Faça um programa que informe a versão do Python que você está utilizando.
 from colorama import Fore, init
 init(autoreset=True)
 import sys
-print(f"Versao do Python {sys.version}")
+print(f"Versao do Python {Fore.BLUE}{sys.version}")
 
 print(f"Informação da versao instalada {sys.version_info}")
