b3251597b10b1fd237e2b4e697a0c6bfd3701234
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-30 21:43:05 -0300

    Update ex94_numeros divisiveis.py

--- PARTE 6/ex94_numeros divisiveis.py
+++ PARTE 6/ex94_numeros divisiveis.py
@@ -1,7 +1,8 @@
 ''''
 Credito : https://www.huicode.com.br/2020/09/ler-um-valor-inteiro-aceitar-somente.html ref: Ex6
 
-Escreva um programa que mostre todos os números entre 5 e 100 que são divisíveis por 7, mas não são múltiplos de 5. Os números obtidos devem ser impressos em sequência.
+Escreva um programa que mostre todos os números entre 5 e 100 que são divisíveis por 7, mas não são múltiplos de 5. 
+Os números obtidos devem ser impressos em sequência.
 '''
 for num in range(5, 100):
     if (num % 7 == 0 and num % 5 != 0):
