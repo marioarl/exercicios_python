70cb479f76167d2cae30fd2048fbd4bc023d7124
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-05 07:34:08 -0300

    Update ex12_produto mais barato.py

--- PARTE 1/ex12_produto mais barato.py
+++ PARTE 1/ex12_produto mais barato.py
@@ -2,6 +2,7 @@
 Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex8
 Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 '''
+
 from colorama import Fore, init
 init(autoreset=True)
 prodA = float(input(Fore.YELLOW + 'Preço do Produto A: R$ ' + Fore.RESET))
