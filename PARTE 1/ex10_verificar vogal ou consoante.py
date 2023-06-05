7eaf6a071f6edf75caa1a3b71e2c808e9eac5125
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-05 07:32:58 -0300

    Update ex10_verificar vogal ou consoante.py

--- PARTE 1/ex10_verificar vogal ou consoante.py
+++ PARTE 1/ex10_verificar vogal ou consoante.py
@@ -2,6 +2,7 @@
 Credito : https://wiki.python.org.br/EstruturaDeDecisao Ex4
 Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 '''
+
 letra = str(input('Digite uma letra: ')).strip().upper()[0]
 print(f'A letra {letra} é ', end="")
 if letra in "AEIOU":
