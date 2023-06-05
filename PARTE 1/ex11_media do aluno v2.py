c2a0dfd88ec7db1a1d22d34ff4381f6e294f688a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-05 07:33:27 -0300

    Update ex11_media do aluno v2.py

--- PARTE 1/ex11_media do aluno v2.py
+++ PARTE 1/ex11_media do aluno v2.py
@@ -5,6 +5,7 @@ A mensagem "Aprovado", se a média alcançada for maior ou igual a 7;
 A mensagem "Reprovado", se a média for menor do que 7;
 A mensagem "Aprovado com Distinção", se a média for igual a dez.
 '''
+
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota:  '))
 media = (n1+n2) / 2
