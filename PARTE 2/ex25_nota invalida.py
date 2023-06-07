5bfc34435d430cbb6039d1f8fac34cd2819ab1f7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-07 08:18:23 -0300

    Update ex25_nota invalida.py

--- PARTE 2/ex25_nota invalida.py
+++ PARTE 2/ex25_nota invalida.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex1
 Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido
 e continue pedindo até que o usuário informe um valor válido.
 '''
+
 from colorama import Fore, init
 init(autoreset=True)
 while True:
