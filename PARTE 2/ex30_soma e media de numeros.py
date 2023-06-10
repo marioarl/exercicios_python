5892c2dc32339ecc12e689725439e94921bf74f7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-10 18:19:05 -0300

    Update ex30_soma e media de numeros.py

--- PARTE 2/ex30_soma e media de numeros.py
+++ PARTE 2/ex30_soma e media de numeros.py
@@ -2,6 +2,7 @@
 Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex8
 Faça um programa que leia 5 números e informe a soma e a média dos números
 '''
+
 media = soma = 0
 
 for n in range(1,6):
