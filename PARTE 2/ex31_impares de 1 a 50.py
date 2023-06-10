48adff2f2697f5c769d794bcb84822dd06b3a772
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-10 18:19:20 -0300

    Update ex31_impares de 1 a 50.py

--- PARTE 2/ex31_impares de 1 a 50.py
+++ PARTE 2/ex31_impares de 1 a 50.py
@@ -2,6 +2,7 @@
 Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex9
 Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50
 '''
+
 #primira opcao
 for i in range(1,51,2):
     print(f'{i} ', end="")
