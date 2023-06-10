90977ea681d52f196b2f25f21335eeaef13b6aea
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-10 18:20:41 -0300

    Update ex34_apuracao de eleicao.py

--- PARTE 2/ex34_apuracao de eleicao.py
+++ PARTE 2/ex34_apuracao de eleicao.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/EstruturaDeRepeticao Ex26
 Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. 
 Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
 '''
+
 from colorama import init, Fore, Style
 from os import system
 init(autoreset=True)
