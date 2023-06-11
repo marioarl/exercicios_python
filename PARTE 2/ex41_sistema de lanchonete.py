5fb9792ad53180529e130570242efa7b0227e9d4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-11 12:30:46 -0300

    Update ex41_sistema de lanchonete.py

--- PARTE 2/ex41_sistema de lanchonete.py
+++ PARTE 2/ex41_sistema de lanchonete.py
@@ -15,6 +15,7 @@ Faça um programa que leia o código dos itens pedidos e as quantidades desejada
 a ser pago por item (preço * quantidade) e o total geral do pedido. Considere que o cliente deve informar 
 quando o pedido deve ser encerrado.
 '''
+
 from os import system
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
