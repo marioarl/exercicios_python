b0621b910c3ae047df0bda18ee38d91c8b745b02
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-12 18:40:36 -0300

    Update ex51_media de alunos em uma lista.py

--- PARTE 3/ex51_media de alunos em uma lista.py
+++ PARTE 3/ex51_media de alunos em uma lista.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosListas Ex6
 
 Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
 '''
+
 from os import system
 from colorama import Fore,init
 init(autoreset=True)
