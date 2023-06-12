7b576057533eb1c2dfe072701ca5e211c12ae4ef
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-12 18:41:43 -0300

    Update ex54_alunos abaixo da media altura.py

--- PARTE 3/ex54_alunos abaixo da media altura.py
+++ PARTE 3/ex54_alunos abaixo da media altura.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosListas ref: Ex listas 12
 
 Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
 '''
+
 from os import system
 from colorama import Fore, init
 init(autoreset=True)
