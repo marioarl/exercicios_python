7ec387563c14cb0e6aeeaffd3d5783cb4bc42174
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-12 18:39:47 -0300

    Update ex47_lendo numeros ordem reversa.py

--- PARTE 3/ex47_lendo numeros ordem reversa.py
+++ PARTE 3/ex47_lendo numeros ordem reversa.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosListas Ex2
 
 Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa
 '''
+
 from os import system
 system('cls')
 listNum = []
