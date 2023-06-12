e9515cfe77291267000cda44f7f05ddedbc2d532
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-12 18:40:16 -0300

    Update ex50_pares e impares em uma lista.py

--- PARTE 3/ex50_pares e impares em uma lista.py
+++ PARTE 3/ex50_pares e impares em uma lista.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosListas Ex5
 
 Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 '''
+
 from colorama import Fore, init
 init(autoreset=True)
 numeros = []
