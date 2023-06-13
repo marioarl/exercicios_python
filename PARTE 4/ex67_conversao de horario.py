7a06f33f7ce60596d3d08b9c87031afbc5a0f671
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-13 09:19:30 -0300

    Update ex67_conversao de horario.py

--- PARTE 4/ex67_conversao de horario.py
+++ PARTE 4/ex67_conversao de horario.py
@@ -7,6 +7,7 @@ Deve haver pelo menos duas funções: uma para fazer a conversão e uma para a s
 Registre a informação A.M./P.M. como um valor A para A.M. e P para P.M. 
 Assim, a função para efetuar as conversões terá um parâmetro formal para registrar se é A.M. ou P.M. Inclua um loop que permita que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
 '''
+
 from os import system
 from colorama import Fore, init
 init(autoreset=True)
