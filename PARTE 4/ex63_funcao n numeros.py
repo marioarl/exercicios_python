6fdc3e5224940f651a5d0a3c8790855de946668a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-13 09:17:57 -0300

    Update ex63_funcao n numeros.py

--- PARTE 4/ex63_funcao n numeros.py
+++ PARTE 4/ex63_funcao n numeros.py
@@ -9,6 +9,7 @@ Faça um programa para imprimir:
     n   n   n   n   n   n  ... n
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
 '''
+
 from os import system
 system('cls')
 
