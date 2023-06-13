4de06a02a9b8d262c92ed2d0da12e49c28da85df
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-13 09:18:48 -0300

    Update ex65_func positivo e negativo.py

--- PARTE 4/ex65_func positivo e negativo.py
+++ PARTE 4/ex65_func positivo e negativo.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosFuncoes ref: Ex funcoes 4
 
 Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere P se seu argumento for positivo, e N, se seu argumento for zero ou negativo.
 '''
+
 from os import system
 system('cls')
 
