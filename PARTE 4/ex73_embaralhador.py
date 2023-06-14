83046eeaa5802749bdd6d5e709b1d24023b77d55
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-14 09:47:13 -0300

    Update ex73_embaralhador.py

--- PARTE 4/ex73_embaralhador.py
+++ PARTE 4/ex73_embaralhador.py
@@ -3,6 +3,7 @@ Credito : https://wiki.python.org.br/ExerciciosFuncoes ref: Ex funcoes 12
 
 Embaralha palavra. Construa uma função que receba uma string como parâmetro e devolva outra string com os carateres embaralhados. Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória. Padronize em sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados.
 '''
+
 from os import system
 from colorama import Fore, init
 init(autoreset=True)
