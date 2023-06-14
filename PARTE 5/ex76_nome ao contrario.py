9e32b830b8fd0ba11e6f5488143702fd1d18b809
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-14 09:48:24 -0300

    Update ex76_nome ao contrario.py

--- PARTE 5/ex76_nome ao contrario.py
+++ PARTE 5/ex76_nome ao contrario.py
@@ -5,6 +5,7 @@ Nome ao contrário em maiúsculas.
 Faça um programa que permita ao usuário digitar o seu nome e em seguida mostre o nome do usuário de trás para frente utilizando somente letras maiúsculas.
 Dica: lembre que ao informar o nome o usuário pode digitar letras maiúsculas ou minúsculas.
 '''
+
 from os import system
 from colorama import Fore, init
 init(autoreset=True)
