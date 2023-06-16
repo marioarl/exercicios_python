34d766a01a53ca45c3ab7f9c63464d011767cbea
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-16 09:24:53 -0300

    Update ex91_classe conta corrente.py

--- PARTE 6/ex91_classe conta corrente.py
+++ PARTE 6/ex91_classe conta corrente.py
@@ -5,6 +5,7 @@ Classe Conta Corrente: Crie uma classe para implementar uma conta corrente.
 A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo. Os métodos são os seguintes: alterarNome, depósito e saque.
 No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
 '''
+
 class ContaCorrente:
     def __init__(self,numero,nome,saldo=0):
         self.num = numero
