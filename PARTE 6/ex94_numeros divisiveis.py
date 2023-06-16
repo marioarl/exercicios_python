e007fbe9632a25dfc519c9977f76c6a87b29b43e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-16 09:26:35 -0300

    Update ex94_numeros divisiveis.py

--- PARTE 6/ex94_numeros divisiveis.py
+++ PARTE 6/ex94_numeros divisiveis.py
@@ -4,6 +4,7 @@ Credito : https://www.huicode.com.br/2020/09/ler-um-valor-inteiro-aceitar-soment
 Escreva um programa que mostre todos os números entre 5 e 100 que são divisíveis por 7, mas não são múltiplos de 5. 
 Os números obtidos devem ser impressos em sequência.
 '''
+
 from colorama import Fore, init
 init()
 for num in range(5, 100):
