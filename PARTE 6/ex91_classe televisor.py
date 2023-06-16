ac42845a9a3f56246076a48b0005252c740e435d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-16 09:25:20 -0300

    Update ex91_classe televisor.py

--- PARTE 6/ex91_classe televisor.py
+++ PARTE 6/ex91_classe televisor.py
@@ -5,7 +5,6 @@ Classe TV:
 Faça um programa que simule um televisor criando-o como um objeto. 
 O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume. Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.
 Considerando os canais abertos de TV que vao do 2 até o 13 e o volume do 0 ate 10
-
 '''
 import os
 from os import system
