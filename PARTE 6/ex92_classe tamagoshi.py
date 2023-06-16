ac3e763253924bc7b0578b643409731053056926
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-16 09:26:00 -0300

    Update ex92_classe tamagoshi.py

--- PARTE 6/ex92_classe tamagoshi.py
+++ PARTE 6/ex92_classe tamagoshi.py
@@ -5,6 +5,7 @@ Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletr�
 
 Atributos: Nome, Fome, Saúde e Idade b. Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
 '''
+
 import os
 from os import system
 if os.name == "posix":
