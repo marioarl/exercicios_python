5e6eeb03cc65b38e1bc529a2ecc2ff6b62b8b676
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-16 09:24:08 -0300

    Update ex89_classe retangulo.py

--- PARTE 6/ex89_classe retangulo.py
+++ PARTE 6/ex89_classe retangulo.py
@@ -5,6 +5,7 @@ Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
 Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
 Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades de um local. Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.
 '''
+
 class Retangulo:
     def __init__(self, ladoA, ladoB):
         self.ladoA = ladoA
